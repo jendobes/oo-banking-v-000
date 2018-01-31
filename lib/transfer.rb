a66003fcb3bfac061006bdc430d52f158f6e4c6d
Jennifer Dobak <jdobak1@gmail.com>
2018-01-31 16:37:43 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -7,7 +7,15 @@ class Transfer
     @sender = sender
     @receiver = receiver
     @amount = amount
-
-
   end
+
+  def valid
+  end
+
+  def execute_transaction
+  end
+
+  def reverse_transfer
+  end
+  
 end
