f39dc5559ca0cde4e2516c1496702d4974d3ed8a
Jennifer Dobak <jdobak1@gmail.com>
2018-01-31 16:26:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,6 +4,9 @@ class Transfer
 
   def initialize(sender, receiver, transfer_amt)
     @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @transfer_amt = transfer_amt
 
 
   end
